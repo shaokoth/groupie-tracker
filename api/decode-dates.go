98de9f501fbac873ab85acd343e7fa7d95e010af
joseowino <joeoowino@gmail.com>
2024-09-22 16:08:53 +0300

    feat: Added DecodeDates Function

---
+++ api/decode-dates.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Dates struct {
+	Index []Date
+}
+
+type Date struct {
+	ArtistID int      `json:"id"`
+	Date     []string `json:"Dates"`
+}
+
+func DecodeDates() Dates {
+	apiBody := FetchAPI("https://groupietrackers.herokuapp.com/api/dates")
+	Dates := Dates{}
+
+	err := json.Unmarshal(apiBody, &Dates)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return Dates
+}
