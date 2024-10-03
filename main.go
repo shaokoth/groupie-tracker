01a293052163b021fd62d0b1b963f7db711334e6
shaokoth <shadrackomondi005@gmail.com>
2024-10-03 11:32:10 +0300

    chore: Implement client-server interaction for concert location filtering

--- main.go
+++ main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"groupie-tracker/api"
+	"net/http"
+
+	"groupie-tracker/handlers"
 )
 
 func main() {
-	fmt.Println(string(api.FetchAPI("https://groupietrackers.herokuapp.com/api/artists")))
+	// fmt.Println(string(api.FetchAPI("https://groupietrackers.herokuapp.com/api/artists")))
+	http.HandleFunc("/", handlers.Artisthandler)
+	http.HandleFunc("/dates", handlers.Datehandler)
+	http.HandleFunc("/locations", handlers.Locationhandler)
+	http.HandleFunc("/relations", handlers.Relationthandler)
+
+	http.ListenAndServe(":8080", nil)
 }
